ecedcae1101fe6f62370afa04a7da64b6944c489
= <=>
2023-04-17 21:45:26 -0500

    Mutiple states in the form

--- src/App.js
+++ src/App.js
@@ -3,15 +3,39 @@ import Personal from "./Components/Personal";
 import "./App.css";
 
 function App() {
-  const temp = ["apple", "banana", "Grapes", "Orange"];
-  const [products, setProducts] = useState(temp);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [data, setData] = useState("Default Data");
+
+  const firstNameChangeHandler = (e) => {
+    setFirstName(e.target.value);
+  };
+  const lastNameChangeHandler = (e) => {
+    setLastName(e.target.value);
+  };
+  const showData = (e) => {
+    e.preventDefault();
+    setData(firstName + "," + lastName);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        {products.map((ele, key) => {
-          return <li key={key}>{ele}</li>;
-        })}
-      </header>
+      <form onSubmit={showData}>
+        <input
+          type="text"
+          placeholder="FirstName"
+          onChange={firstNameChangeHandler}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="LastName"
+          onChange={lastNameChangeHandler}
+        />
+        <br />
+        <input type="submit" value="submit" />
+      </form>
+      {data}
     </div>
   );
 }
